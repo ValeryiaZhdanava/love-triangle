b8b00712729e642029485f60c834d6414051d83a
Valeryia Zhdanava <vzhd@ciklum.com>
2018-02-21 00:38:13 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-const log4js = require("log4js");
-const logger = log4js.getLogger('Service logger');
 
 module.exports = function getLoveTrianglesCount(preferences = []) {
     let array = [];
@@ -12,10 +10,8 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
         let second = preferences[first - 1];
         let third = preferences[second - 1];
         if (third === i && first !== second && !array.includes(second)) {
-            // logger.info(first, second, third);
             array.push(first, second, third);
          }
     }
-    logger.info(array.length/3);
     return array.length/3;
 };
